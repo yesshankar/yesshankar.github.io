232c500dacc10bc5c93e18633e13a657592140e5
Shankar Khatri <yes143.shankar@gmail.com>
2019-12-09 11:55:52 -0600

    Add contact form email feature and use prettier

--- js/index.js
+++ js/index.js
@@ -1,24 +1,65 @@
+const contactForm = document.getElementById("contact-form");
+const submitButton = contactForm.querySelector("button[type=submit]");
+const messageStatus = document.getElementById("message-status");
+const apiUrl = "https://api-skhatri-xyz.herokuapp.com/email/contact-form";
+// const apiUrl = "http://localhost:3333/email/contact-form";
 
-document.getElementById('contat-form').addEventListener('click', (e) => {
-    e.preventDefault();
-    alert('Sorry! This feature is yet to implement. Please send me an email at yes143.shankar@gmail.com');
+contactForm.addEventListener("submit", async e => {
+  e.preventDefault();
+  submitButton.setAttribute("disabled", "");
+  messageStatus.innerText = "Sending message........";
+
+  const formData = new FormData(contactForm);
+  const formDataObj = {};
+
+  for (const pair of formData.entries()) {
+    formDataObj[pair[0]] = pair[1];
+  }
+
+  const response = await sendFormData(formDataObj, apiUrl);
+
+  if (response.Status === "success") {
+    messageStatus.innerText = "Message sent successfully!!";
+    messageStatus.style.color = "green";
+  } else {
+    messageStatus.innerText =
+      "Sorry! Unable to send message. Someting went wrong, please contact via other method";
+    messageStatus.style.color = "red";
+  }
+
+  submitButton.removeAttribute("disabled");
+
+  setTimeout(() => {
+    messageStatus.style.visibility = "hidden";
+  }, 10000);
 });
 
-let typingP = document.getElementById('typing');
+async function sendFormData(data, url) {
+  const response = await fetch(apiUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  });
+
+  return await response.json();
+}
+
+let typingP = document.getElementById("typing");
 
 typeWord(typingP, "{Software_Developer}");
 
 setInterval(() => {
-    typeWord(typingP, "{Software_Developer}");
+  typeWord(typingP, "{Software_Developer}");
 }, 5000);
 
-function typeWord(elem, word){
-    elem.innerText = word.charAt(0);
+function typeWord(elem, word) {
+  elem.innerText = word.charAt(0);
 
-    for (let index = 1; index < word.length; index++) {
-        
-        setTimeout(() => {
-            elem.innerText += word.charAt(index);
-        }, (index +1) * 100);
-    }
+  for (let index = 1; index < word.length; index++) {
+    setTimeout(() => {
+      elem.innerText += word.charAt(index);
+    }, (index + 1) * 100);
+  }
 }
